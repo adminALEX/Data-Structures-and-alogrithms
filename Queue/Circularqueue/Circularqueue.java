9653f57884b044f1c36f817fcfc6816814bf05ca
adminAlex <alexflora6@gmail.com>
2022-08-28 21:36:10 +0530

    circular queue

--- Queue/Circularqueue/Circularqueue.java
+++ Queue/Circularqueue/Circularqueue.java
@@ -1,5 +1,36 @@
 package Queue.Circularqueue;
 
+import java.util.Arrays;
+
 public class Circularqueue {
+
+    int q[];
+    int qsize;
+    int rear;
+    int front;
+
+    Circularqueue(int l){
+        q = new int[l];
+    }
+    public void enqueue(int i) {
+        if(qsize==q.length)
+            throw new StackOverflowError("Stack is Full");
+        q[rear] = i;
+        rear = (rear+1)%q.length;
+        qsize++;
+    }
+
+    public void dequeue() {
+        q[front] = 0;
+        front = (front+1)%q.length;
+        qsize--;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(q);
+    }
+
+    
     
 }
--- Queue/Circularqueue/Main.java
+++ Queue/Circularqueue/Main.java
@@ -1,8 +1,27 @@
 package Queue.Circularqueue;
 
+
 public class Main {
     public static void main(String[] args) {
-        
-        
+        Circularqueue cq = new Circularqueue(6);
+
+        cq.enqueue(12);
+        cq.enqueue(43);
+        cq.enqueue(45);
+        cq.enqueue(22);
+        cq.enqueue(11);
+        System.out.println(cq);
+        cq.dequeue();
+        cq.dequeue();
+        System.out.println(cq);
+        cq.enqueue(78);
+        System.out.println(cq);
+        cq.enqueue(7823);
+        System.out.println(cq);
+        cq.enqueue(56);
+        cq.dequeue();
+        cq.enqueue(65);
+        System.out.println(cq);
+
     }
 }
